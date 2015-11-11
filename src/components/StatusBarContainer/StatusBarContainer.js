8a04ba73b6397baa2d059accd190ece25f1ddf99
John Tran <john.tran@shiphawk.com>
2015-11-11 01:12:08 -0800

    adding status bar

---
+++ src/components/StatusBarContainer/StatusBarContainer.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//StatusBarContainer.scss');
+
+let StatusBarContainer = (props) => (
+  <div className="statusbar-Container">
+
+  </div>
+);
+
+StatusBarContainer.displayName = 'StatusBarContainer';
+
+// Uncomment properties you need
+// StatusBarContainer.propTypes = {};
+// StatusBarContainer.defaultProps = {};
+
+
+export default StatusBarContainer;
---
+++ src/components/StatusBarContainer/StatusNodeComponent.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//StatusNodeComponent.scss');
+
+let StatusNodeComponent = (props) => (
+  <div className="statusbar-Container">
+
+  </div>
+);
+
+StatusNodeComponent.displayName = 'StatusNodeComponent';
+
+// Uncomment properties you need
+// StatusNodeComponent.propTypes = {};
+// StatusNodeComponent.defaultProps = {};
+
+
+export default StatusNodeComponent;
